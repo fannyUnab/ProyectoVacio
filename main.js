0b6ff5fcf9ca683990c3947a19739b06a9ca2883
fannyUnab <fanny.casadiego2@o365.unab.edu.co>
2022-10-19 18:53:39 -0500

    frontend configurado login

--- main.js
+++ main.js
@@ -15,3 +15,35 @@ function otrafuncion(){
     document.getElementById('dos').innerHTML=x;
 
 }
+
+function validar(){
+   let nombre=document.formudatos.querySelector("#validationCustom01").value;
+   console.log(nombre);
+   let lonombre=document.formudatos.querySelector("#validationCustom01").value.length;
+   console.log(lonombre);
+    if(lonombre<8){
+        alert("ingrese un usuario mayor a 8 ");
+    }
+    else{
+        document.location="login.html";
+    }
+
+}
+
+function verificar(){
+    let correo=document.getElementById("correo").value;
+    let password=document.getElementById("password").value;
+    console.log(correo+"."+password)
+
+    fetch('http://localhost:8080/usuarios/login?correo='+correo+'&password='+password+'')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        if(data.msj=="ok") {
+            console.log("continuar")
+            document.location="productos.html"
+         } else { alert(data.msj) } 
+        } 
+        )
+    .catch(error => console.log(error))    
+}
